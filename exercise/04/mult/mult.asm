f869362db84cebcaa04500298a7daa860d73073c
tony8880221 <36975348+tony8880221@users.noreply.github.com>
2019-01-09 23:32:25 +0800

    Add files via upload

--- exercise/04/mult/mult.asm
+++ exercise/04/mult/mult.asm
@@ -7,3 +7,33 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+  @R2
+  M=0
+
+  //If R0 is zero, end program
+  @R0
+  D=M
+  @END
+  D;JEQ
+
+(ADDAGAIN)
+  //Check if R1 is 0, meaning no more iterations
+  @R1
+  D=M
+  @END
+  D;JEQ
+  
+  //R2 += R0
+  @R0
+  D=M
+  @R2
+  M=D+M
+  
+  @R1
+  M=M-1
+  
+  @ADDAGAIN
+  0;JMP
+(END)
+  @END
+  0;JMP
